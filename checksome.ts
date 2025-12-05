ddc191bbb0ca9e2500532b3d320f6854c35818fa
Leon Bryan <leonard.bryan.email@gmail.com>
2025-12-06 02:08:54 +0800

    Updated checksome.ts, AGAIN

--- checksome.ts
+++ checksome.ts
@@ -7,82 +7,67 @@ interface ServersI {
   rootAccess: boolean;
   securityLvl: number;
   minSecurityLvl: number;
+  hTime: number;
 }
 
 export async function main(ns: NS) {
   let hosts = new Set(["home"]);
   let excludes = ["home", "One", "CSEC", "neo-net"];
 
-  hosts.forEach((host) => {
-    ns.scan(host).forEach((n) => hosts.add(n));
+  hosts.forEach(host => {
+    ns.scan(host).forEach(n => hosts.add(n));
   });
 
   let servers: ServersI[] = [];
 
-  let s = Array.from(hosts).filter((server) => !excludes.includes(server));
+  let s = Array.from(hosts).filter(server => !excludes.includes(server));
 
   for (const target of s) {
     servers.push({
       name: target,
       totalRam: ns.getServerMaxRam(target),
-      availRam: roundOffTwoDec(
-        ns.getServerMaxRam(target) - ns.getServerUsedRam(target)
-      ),
+      availRam: roundOffTwoDec((ns.getServerMaxRam(target) - ns.getServerUsedRam(target))),
       reqHackLvl: ns.getServerRequiredHackingLevel(target),
       availMoney: roundOffTwoDec(ns.getServerMoneyAvailable(target)),
       rootAccess: ns.hasRootAccess(target),
       securityLvl: roundOffTwoDec(ns.getServerSecurityLevel(target)),
       minSecurityLvl: ns.getServerMinSecurityLevel(target),
-    });
+      hTime: roundOffTwoDec(ns.getHackTime(target) / 1000 / 60),
+    })
   }
+
 
   servers.sort((a, b) => a.reqHackLvl - b.reqHackLvl);
 
   // Servers to be pwnd
-  ns.tprint(
-    "******************************* WITHOUT ACCESS *******************************"
-  );
+  ns.tprint("******************************* WITHOUT ACCESS *******************************");
   for (const tbd of servers) {
-    if (!tbd.rootAccess && tbd.reqHackLvl <= ns.getHackingLevel() + 150) {
-      ns.tprint(
-        `${tbd.name} : ${tbd.totalRam}GB : \$${tbd.availMoney} : LVL${tbd.reqHackLvl}`
-      );
+    if (!tbd.rootAccess && tbd.reqHackLvl <= (ns.getHackingLevel() + 150)) {
+      ns.tprint(`${tbd.name} : ${tbd.totalRam}GB : \$${tbd.availMoney} : LVL${tbd.reqHackLvl}`);
     }
   }
-  ns.tprint(
-    "***************************************************************************\n\n"
-  );
+  ns.tprint("***************************************************************************\n\n");
 
   // Servers with Root Access
-  ns.tprint(
-    "******************************* WITH ACCESS *******************************"
-  );
+  ns.tprint("******************************* WITH ACCESS *******************************");
   for (const hkd of servers) {
     if (hkd.rootAccess) {
-      ns.tprint(
-        `${hkd.name} : ${hkd.availRam}GB : \$${hkd.availMoney} : Security LVL of ${hkd.securityLvl} (min ${hkd.minSecurityLvl})`
-      );
+      ns.tprint(`${hkd.name} : ${hkd.availRam}GB : \$${hkd.availMoney} : Security LVL of ${hkd.securityLvl} (min ${hkd.minSecurityLvl})`);
     }
   }
-  ns.tprint(
-    "***************************************************************************\n\n"
-  );
+  ns.tprint("***************************************************************************\n\n");
 
   // Hackable servers
-  ns.tprint(
-    "******************************* READY TO GET MILKED *******************************"
-  );
+  ns.tprint("******************************* READY TO GET MILKED *******************************");
   for (const mlk of servers) {
     if (mlk.rootAccess && mlk.availMoney > 0) {
-      ns.tprint(
-        `${mlk.name} is ready to get milked with \$${mlk.availMoney} inside.`
-      );
+      ns.tprint(`${mlk.name} have \$${mlk.availMoney} inside (${mlk.hTime}min/s).`);
     }
   }
-  ns.tprint(
-    "***********************************************************************************"
-  );
+  ns.tprint("***********************************************************************************");
 
+
+  //Old Code LMAO
   /*
   let hitlist = Array.from(hosts).filter(server => ns.hasRootAccess(server)).filter(server => !excludes.includes(server));
   let availableTargets: string[] = [];
